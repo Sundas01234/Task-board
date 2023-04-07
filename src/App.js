7a7492fb732e233259e0f33466738b27177f0e6d
sundas01234 <sundasrani01234@gmail.com>
2023-04-07 00:44:23 -0700

    create the website and get the response of API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+
+const App = () => {
+  
+  const [users, setUsers] = useState([])
+
+  const fetchUserData = () => {
+    fetch("https://api.coingecko.com/api/v3/coins/list?include_platform=true")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data)
+      })
+     
+  }
+  useEffect(() => {
+    fetchUserData()
+  }, [])
+  //console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {users.length > 0 && (
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.name}{user.symbol}</li>
+          ))}
+        </ul>
+      )}
+    
     </div>
   );
 }
 
 export default App;
+
