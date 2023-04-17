cf1b35b1e99e4cd8430be1dd9f62310c715a635a
sundas01234 <sundasrani01234@gmail.com>
2023-04-17 03:12:47 -0700

    show the data when we click the coin

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 const App = () => {
-
-  const [data, setData] = useState('');
+  
+ const [data, setData] = useState('');
+ const HandleClick = () => {
+  console.log(data)
+ }
+  
   const getAllData = () => {
     axios
-      .get("https://api.coingecko.com/api/v3/coins/list?include_platform=true")
+      .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
       .then((response) => {
         console.log(response.data);
         setData(response.data);
@@ -21,31 +25,22 @@ const App = () => {
 
   return (
     <div className="App">
+      
       {data ?
         data.map(data => {
           return (
-            <div className="data" key={data.id}>
-              <h3>{data.name}</h3>
-            </div>
+            <button onClick={HandleClick() }>coin</button>
           )
-        }) : <h3>coins</h3>}
+        }) : <form>
+          <div></div>
+        </form>}
 
     </div>
   );
+      
 }
 
+
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
 
 
