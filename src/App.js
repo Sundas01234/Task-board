ca75b3e271ddf3e914a3eab40342808bff1635e2
sundas01234 <sundasrani01234@gmail.com>
2023-04-25 05:53:38 -0700

    display the coin

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 const App = () => {
-  
+ 
  const [data, setData] = useState('');
- const HandleClick = () => {
-  console.log(data)
- }
+ 
   
   const getAllData = () => {
     axios
-      .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
+      .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=1&page=1&sparkline=false&locale=en")
       .then((response) => {
         console.log(response.data);
         setData(response.data);
@@ -23,23 +21,25 @@ const App = () => {
     getAllData();
   }, []);
 
-  return (
-    <div className="App">
-      
-      {data ?
-        data.map(data => {
-          return (
-            <button onClick={HandleClick() }>coin</button>
-          )
-        }) : <form>
-          <div></div>
-        </form>}
-
-    </div>
+const displayData = () => {
+    return data ? (
+      data.map((data) => {
+        return (
+          <div className="data" key={data.id}>
+            <h3>{data.vs_currency}</h3>
+          </div>
+        );
+      })
+    ) : (
+      <h3>No data yet</h3>
+    );
+  }
+return (
+    <>
+      {displayData()}
+    </>
   );
-      
 }
-
 
 export default App;
 
